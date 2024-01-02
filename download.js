f27dea6e353d44b87b1231d047cfb26572053585
Taisuke Fukuno <fukuno@jig.jp>
2024-01-03 00:10:35 +0900

    update data

--- download.js
+++ download.js
@@ -1,17 +1,7 @@
 import { DateTime, Time, Day, TimeZone } from "https://js.sabae.cc/DateTime.js";
 import { CSV } from "https://js.sabae.cc/CSV.js";
-import { Station } from "./Station.js";
+import { save } from "./save.js";
 
-export const save = async (id, date) => {
-  const st = new Station(id, date);
-  //const url = "https://www.jma.go.jp/bosai/tidelevel/data/tide/tide_obs_20231226_145553.json";
-  const fn = st.getName();
-  const url = `https://www.jma.go.jp/bosai/tidelevel/data/tide/${fn}`;
-  const data = await (await fetch(url)).text();
-  const path = st.getPath();
-  await Deno.mkdir(path, { recursive: true });
-  await Deno.writeTextFile(path + fn, data);
-};
 
 const stations = await CSV.fetchJSON("data/stations.csv");
 
@@ -23,6 +13,7 @@ export const saveDay = async (day) => {
   }
 };
 
+/*
 const now = new DateTime();
 const day2 = new DateTime(new Day("2024/01/02"), new Time("00:00:00"), TimeZone.JST);
 const dt = day2.getTime() - now.getTime();
@@ -32,3 +23,7 @@ if (dt > -30 * 60 * 1000) {
 if (dt < 0) {
   await saveDay(new Day("2024/01/02"));
 }
+*/
+//await saveDay(new Day("2024/01/01"));
+//await saveDay(new Day("2024/01/02"));
+await saveDay(new Day("2024/01/03"));
