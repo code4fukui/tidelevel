e3068c17da5df288ab61edc1417c813b87c1c0ce
Taisuke Fukuno <fukuno@jig.jp>
2024-01-04 09:03:43 +0900

    fix path

--- saveDay.js
+++ saveDay.js
@@ -3,7 +3,7 @@ import { save } from "./save.js";
 import { sleep } from "https://js.sabae.cc/sleep.js";
 
 //const stations = await CSV.fetchJSON("data/stations.csv");
-const stations = await CSV.fetchJSON("./stations3.csv");
+const stations = await CSV.fetchJSON("./stations.csv");
 
 export const saveDay = async (day) => {
   console.log("saveDay", day);
